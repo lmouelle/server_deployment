9e0704d70d4e83caa83044f3c00c07c111ff3a94
Lucas Ouellette-Falkenstein <fake@lies.com>
2024-04-13 03:48:34 -0700

    simplify

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 # Any random host setup required, like SELinux permissions
 # Eventually move this into a proper Ansible config, a script is fine for now though
 
-# Plan, options, techs:
-# quadlets (https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html)
-# let me integrate systemd in and easily manage with host OS, logical
-# choice for restic type automations on a timer. I see no 'network_mode'
-# feature on sight to easily route deluge traffic through the gluetun container though.
-# podman compose does not support secrets into container env vars, and is awkward
-# for stuff like restic that should not run on compose up but on a timer schedule.
-# Ansible is complex but can do everything, prob replace these script for stuff like
-# SELinux settings. Or just a long ass shell script?
-
 # These two are needed by gluetun container, the former
 # to access the /dev/net/tun device and the latter so the container
 # may load/read firewall rules with nftables module
@@ -25,40 +15,23 @@ setsebool -P domain_kernel_load_modules=true
 # the gluetun container needs nothing in compose.yml, no luck.
 # Will just set the SELinux booleans above instead
 
-# We also have file/user permissions on the /data directory, anything from me there?
-
-# Any kind of NFS/Samba share setup here as well
-
-# This is required for quadlets, https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html#network
-# They only scan these paths for non-root containers:
-# $XDG_CONFIG_HOME/containers/systemd/ or ~/.config/containers/systemd/
-# /etc/containers/systemd/users/$(UID)
-# /etc/containers/systemd/users/
-# and /etc/containers/systemd for root containers
-# TODO: Can I use something like gnu stow for symlink management here?
-
-
-# Symlink the quadlet files to the UID of each new user
-# ... Or root user can start containers, but the containers run as nonroot?
+# Any kind of host NFS/Samba share setup here as well
 
 for container_name in (ls */*.container | cut -f1 -d/); do
-    useradd -rmU $container_name -s /sbin/nologin
+    useradd -rU $container_name -s /sbin/nologin -M -d ./$container_name/
     usermod luouelle -aG $container_name
-    # TODO: Consider replacing this with gnu stow
-    mkdir -p /home/$container_name/.config/containers/systemd/
-    ln -s $container_name/$container_name.container /home/$container_name/.config/containers/systemd/
     # reloading every time is inefficient but we want systemctl enable to see the current file
     systemctl daemon-reload
-    systemctl enable --now /home/$container_name/.config/containers/systemd/$container_name.container --user $container_name
+    systemctl enable --now $container_name --user $container_name
 
-    # TODO: systemctl enable the user defined podman service file
-    # TODO: Create timer for restic, and volume/image defs in each
+    # TODO: Create timer for restic, and volume/image defs in each service
     # TODO: Have not 1 restic script that runs backup/check/prune/check but 4
     # systemd units, each going to the next only on success. This way I do not need
     # the container to exec scripts, just run restic
 done
 
 # TODO: Add media servers like Calibre and Jellyfin here
+# Can I write some manifest in each app/service/container and have it say what host permissions it needs?
 chown -R deluge:deluge /data/torrents 
 
 # Need to do this so everything works? Unsure how to propogate podman secrets
