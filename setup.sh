b68f523d2f127af094a70bf8f2593c462d67db2c
Lucas Ouellette-Falkenstein <fake@lies.com>
2024-04-15 15:13:00 -0700

    extract some vars

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Maybe use gnu stow + a makefile
 
 me=luouelle
+datadir=$datadir
 
 # Host DNF packages. We assume git is already installed else this script
 # would not be on the host, but for completeness I include it here
@@ -25,28 +26,30 @@ setsebool -P domain_kernel_load_modules=true
 # Any kind of host NFS/Samba share setup here as well
 
 for container_name in (ls */.config/* | cut -f1 -d/); do
-    useradd -rU $container_name -s /usr/sbin/nologin -m
-    usermod luouelle -aG $container_name
+    useradd -rU $container_name -m
+    usermod $me -aG $container_name
     loginctl enable-linger $container_name
     # TODO: Any SELinux file specific context to set here?
-    chown -R -c luouelle:$container_name $container_name/ --preserve-root
+    chown -R -c $me:$container_name $container_name/ --preserve-root
     chmod 0760 -R --preserve-root -c $container_name/
     stow --target=/home/$container_name --stow --dotfiles $container_name
 done
 
 # Intentionally nonrecursive here.
-groupadd data -f -r -U restic luouelle
-chown -Rc --preserve-root luouelle:data /data
+groupadd data -f -r -U restic $me
+chown -Rc --preserve-root $me:data $datadir
 chmod -Rc --preserve-root 0760
 
-# Overwrite perms set above for /data/{comics,videos}
+# Overwrite perms set above for $datadir/{comics,videos}
 # TODO: Add media servers like Calibre and Jellyfin here, and *arr apps.
 # Can I write some manifest in each app/service/container and have it say what host permissions it needs?
-# May create 'videos', 'comics' groups for different container/users to access stuff like /data/videos
+# May create 'videos', 'comics' groups for different container/users to access stuff like $datadir/videos
 # Feels like I'm recreating Ansible badly here...
-groupadd torrents -f -r -U deluge restic luouelle
-chown -Rc --preserve-root luouelle:torrents /data/torrents 
+groupadd torrents -f -r -U deluge restic $me
+chown -Rc --preserve-root $me:torrents $datadir/torrents 
 chmod -Rc --preserve-root 0760
+
+semanage fcontext $datadir -a -t container_file_t 
 
 # TODO: Does the section below need to run as the container account?
 # I believe so, test this. Do i need to run su with -l to simulate user login?
@@ -57,5 +60,5 @@ done
 
 # Need to do this so everything works? Unsure how to propogate podman secrets
 # TODO: Eventually bootstrap secrets with bitwarden sync
-# cat /data/deployment/container_configs/restic/.restic_passwd | podman secret create restic-repo-password -
-# cat /data/deployment/container_configs/restic/.aws_secret_access_key | podman secret create aws-secret-access-key -
+# cat $datadir/deployment/container_configs/restic/.restic_passwd | podman secret create restic-repo-password -
+# cat $datadir/deployment/container_configs/restic/.aws_secret_access_key | podman secret create aws-secret-access-key -
