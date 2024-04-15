dfc25439f4dd23942edefc59018d33025253a5e1
Lucas Ouellette-Falkenstein <fake@lies.com>
2024-04-15 14:50:06 -0700

    Script tweaks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 # Any random host setup required, like SELinux permissions
-# Eventually move this into a proper Ansible config, a script is fine for now though
+# Eventually move this into a proper Ansible config, a script is fine for now though.
+# Maybe use gnu stow + a makefile
+
+me=luouelle
+
+# Host DNF packages. We assume git is already installed else this script
+# would not be on the host, but for completeness I include it here
+dnf install podman fish git stow # ansible (will I ever move to ansible?)
 
 # These two are needed by gluetun container, the former
 # to access the /dev/net/tun device and the latter so the container
@@ -17,21 +24,36 @@ setsebool -P domain_kernel_load_modules=true
 
 # Any kind of host NFS/Samba share setup here as well
 
-for container_name in (ls */.config/containers/systemd*.container | cut -f1 -d/); do
+for container_name in (ls */.config/* | cut -f1 -d/); do
     useradd -rU $container_name -s /usr/sbin/nologin -m
     usermod luouelle -aG $container_name
-    mkdir -p /home/$container_name/.config/containers
-    ln -s $container_name/.config/containers/systemd/ /home/$container_name/.config/containers/
-    # reloading every time is inefficient but we want systemctl enable to see the current file
-    systemctl daemon-reload --user
-    systemctl enable $container_name --user --now
+    loginctl enable-linger $container_name
+    # TODO: Any SELinux file specific context to set here?
+    chown -R -c luouelle:$container_name $container_name/ --preserve-root
+    chmod 0760 -R --preserve-root -c $container_name/
+    stow --target=/home/$container_name --stow --dotfiles $container_name
 done
 
+# Intentionally nonrecursive here.
+groupadd data -f -r -U restic luouelle
+chown -Rc --preserve-root luouelle:data /data
+chmod -Rc --preserve-root 0760
+
+# Overwrite perms set above for /data/{comics,videos}
 # TODO: Add media servers like Calibre and Jellyfin here, and *arr apps.
 # Can I write some manifest in each app/service/container and have it say what host permissions it needs?
 # May create 'videos', 'comics' groups for different container/users to access stuff like /data/videos
-groupadd torrents -f -r -U deluge restic
-chown -R deluge:torrents /data/torrents 
+# Feels like I'm recreating Ansible badly here...
+groupadd torrents -f -r -U deluge restic luouelle
+chown -Rc --preserve-root luouelle:torrents /data/torrents 
+chmod -Rc --preserve-root 0760
+
+# TODO: Does the section below need to run as the container account?
+# I believe so, test this. Do i need to run su with -l to simulate user login?
+for container_name in (ls */.config/containers/systemd*.container | cut -f1 -d/); do
+    su $container_name -c 'systemctl daemon-reload --user' -l
+    su $container_name -c 'systemctl enable $container_name --user --now' -l
+done
 
 # Need to do this so everything works? Unsure how to propogate podman secrets
 # TODO: Eventually bootstrap secrets with bitwarden sync
