a1d3aa8418268c3f07e1e1af2c9268721930b98a
Lucas Ouellette-Falkenstein <fake@lies.com>
2024-04-23 13:26:17 -0700

    Fix chown directory bug

--- setup.sh
+++ setup.sh
@@ -58,8 +58,8 @@ for package in $(ls */dot-config/* -d | cut -f1 -d/); do
     chmod 750 -Rc --preserve-root $package/
     # This script must be run as root, which runs stow as root and creates symlinks with
     # root:root as owner:group. Fix that immediately after
-    target_dir=$(userdbctl user $package --output=classic | cut -f5 -d:)
-    stow --target=$target_dir --stow --dotfiles $package/ -vvv
+    target_dir=$(userdbctl user $package --output=classic | cut -f6 -d:)
+    stow --target=$target_dir --stow --dotfiles $package/ -vvvvv
     chown -Rc -h -P $package:$package $target_dir --preserve-root --from=root:root
 done
 
