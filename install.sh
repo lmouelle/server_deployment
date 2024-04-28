11d161770121a242f106d73599f3c352ed838b6b
Lucas Ouellette-Falkenstein <fake@lies.com>
2024-04-28 14:17:56 -0700

    Install script should set BW token

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-# bitwarden setup for initial secrets
+#!/usr/bin/bash
+set -euo pipefail
+
+# bitwarden setup for initial secrets, set BWS_ACCESS_TOKEN=<your_access_token> or pass it in with ansible vault?
+# https://galaxy.ansible.com/ui/repo/published/bitwarden/secrets/docs/
+
+# I want to avoid using ansible vault, and maybe even podman secrets, if I can
+# Just put everything in bitwarden and pull it with the ansible module from bitwarden
+export HISTCONTROL=ignorespace
+# Leading space is important here!
+ export BWS_ACCESS_TOKEN=<token>
 
 # Install dependencies for ansible run
 ansible-galaxy collection install -r requirements.ansible.yml
 
-# This pre-assumes there is a file called secrets.enc that was encrypted with 
-# ansible-vault encrypt secrets.enc.
-# secrets.enc will be a yml file with name: value format
-# ansible-playbook -i inventory.ini -e @secrets.enc --ask-
-
+# After install and setup, some remaining tasks that I have not/can not automate:
+# Set allow_remote to true for deluge core.conf, https only to true for web conf, create auth file for remote user in bitwarden,
+# create web UI password, etc
+# Maybe create an ansible module for that? deluge was designed to generate auth/core.conf/web.conf on first run, makes it harder
