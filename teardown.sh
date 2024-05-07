03b0dbb623e57f4958f81ed9ef1d31ba10e2b64b
Lucas Falkenstein <lmouelle@users.noreply.github.com>
2024-05-06 18:53:07 -0700

    Add teardown script

--- teardown.sh
+++ teardown.sh
@@ -1,32 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
 
-# TODO: Replace this with some new ansible playbook where state: absent
-
-for package in $(ls files/* -d | cut -f2 -d/); do
-    if userdbctl user $package &> /dev/null
-    then 
-        if loginctl user-status $package &> /dev/null
-        then
-            loginctl terminate-user $package
-        fi
-
-        userdel $package -rZ
-    fi
-
-    if userdbctl group $package &> /dev/null
-    then
-        groupdel $package -f
-    fi
-done
-
-if userdbctl group data &> /dev/null
-then    
-    groupdel data -f 
-fi
-
-# common_user=$(cat deployment_user_name.txt)
-# groupdel $common_user -f
-
-printf 'Completed teardown\n'
+export HISTCONTROL=ignorespace
+# IIRC leading space here is important to block secrets from leaking
+ ansible-playbook --inventory inventory.ini teardown.ansible.yml --ask-become-pass --vault-password-file secrets/.vault_passwd
